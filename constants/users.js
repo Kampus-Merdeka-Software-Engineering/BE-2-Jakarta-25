6b4a6d59d9ff43040bc9dd8585ee2c4538078617
Jonatan Hutahaean <2010631170149@student.unsika.ac.id>
2023-12-07 13:14:35 +0700

    nambahin user constant

---
+++ constants/users.js
@@ -0,0 +1,12 @@
+export const users = {
+    1: {
+        nama: "Jonatan Hutahaean",
+        email: "jonatanhutahean03@gmail.com",
+        password: "asiap123",
+    },
+    2: {
+        nama: "Veronica",
+        email: "veronica@gmail.com",
+        password: "mantap123",
+    },
+};
