7914a48398e4b012e376bd3abe1db3d0030c9a89
Jonatan Hutahaean <2010631170149@student.unsika.ac.id>
2023-12-07 13:20:21 +0700

    ubah readuser

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,10 +1,9 @@
 import express, {request, response} from "express";
-import { getUsers, postUserItem } from "../controllers/users-controller.js";
+import { postUserItem } from "../controllers/users-controller.js";
 
 
 const router = express.Router();
 
-router.get("/", getUsers);
 router.post("/create", postUserItem);
 
 export default router;
